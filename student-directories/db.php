f36f7c5dfbc61d6638ed84d3a2d5bb6acc2afe76
shakshamdubey007 <bggiarmageddon2024@gmail.com>
2025-08-09 11:52:40 +0530

    Delete student-directories directory

--- student-directories/db.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "student_directory");
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-?>
--- student-directories/hash.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo password_hash("$2y$10$tVSG6bd5ybNHll9vpuQBsuCQbKuKKHBY.i.lwv6pteReyiaNABFRu", PASSWORD_DEFAULT);
-?>
